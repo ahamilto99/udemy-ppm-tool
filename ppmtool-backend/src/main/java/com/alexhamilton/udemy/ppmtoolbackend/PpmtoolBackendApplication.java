0922b795d0dd6f60c192ccc3bf942386c25301ab
ahamilto99 <ajhamilton15@hotmail.com>
2021-08-24 19:51:53 -0400

    Init commit

---
+++ ppmtool-backend/src/main/java/com/alexhamilton/udemy/ppmtoolbackend/PpmtoolBackendApplication.java
@@ -0,0 +1,13 @@
+package com.alexhamilton.udemy.ppmtoolbackend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PpmtoolBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PpmtoolBackendApplication.class, args);
+	}
+
+}
---
+++ ppmtool-backend/src/test/java/com/alexhamilton/udemy/ppmtoolbackend/PpmtoolBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.alexhamilton.udemy.ppmtoolbackend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PpmtoolBackendApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
