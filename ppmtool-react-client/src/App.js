cf59b3201448a9986272e21d2cc3fb3499663c47
ahamilto99 <ajhamilton15@hotmail.com>
2021-08-28 09:30:40 -0400

    Added Header component

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -1,10 +1,11 @@
-import logo from './logo.svg';
 import './App.css';
 import Dashboard from './components/Dashboard';
+import Header from './components/layout/Header';
 
 function App() {
   return (
-    <div className="App">
+    <div className='App'>
+      <Header />
       <Dashboard />
     </div>
   );
--- ppmtool-react-client/src/components/Dashboard.js
+++ ppmtool-react-client/src/components/Dashboard.js
@@ -1,12 +1,17 @@
 import React, { Component } from 'react';
+import ProjectItem from './project/ProjectItem';
 
 class Dashboard extends Component {
-    render() {
-        return (
-            <h1>Welcome to the Dashboard</h1>
-            
-        )
-    }
+  render() {
+    return (
+      <div>
+        <h1>Welcome to the Dashboard</h1>
+        <ProjectItem />
+        <ProjectItem />
+        <ProjectItem />
+      </div>
+    );
+  }
 }
 
 export default Dashboard;
