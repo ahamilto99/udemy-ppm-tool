9f65c500824d1751b68c2324654778c883bddbf1
ahamilto99 <ajhamilton15@hotmail.com>
2021-08-28 13:58:04 -0400

    Created Redux store

--- ppmtool-react-client/src/App.js
+++ ppmtool-react-client/src/App.js
@@ -4,16 +4,20 @@ import './App.css';
 import Dashboard from './components/Dashboard';
 import Header from './components/layout/Header';
 import AddProject from './components/project/AddProject';
+import { Provider } from 'react-redux';
+import store from './store';
 
 function App() {
   return (
-    <Router>
-      <div className='App'>
-        <Header />
-        <Route exact path='/dashboard' component={Dashboard} />
-        <Route exact path='/addProject' component={AddProject} />
-      </div>
-    </Router>
+    <Provider store={store}>
+      <Router>
+        <div className='App'>
+          <Header />
+          <Route exact path='/dashboard' component={Dashboard} />
+          <Route exact path='/addProject' component={AddProject} />
+        </div>
+      </Router>
+    </Provider>
   );
 }
 
