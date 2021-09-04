9d0b22571d5bc8012c309b67b474143240d7fa09
ahamilto99 <ajhamilton15@hotmail.com>
2021-09-04 11:35:48 -0400

    Completed implementation for adding a Project Task

--- ppmtool-react-client/src/actions/backlogActions.js
+++ ppmtool-react-client/src/actions/backlogActions.js
@@ -1,7 +1,19 @@
 import axios from 'axios';
+import { GET_ERRORS } from './types';
 
 export const addProjectTask =
   (backlogId, projectTask, history) => async (dispatch) => {
-    await axios.post(`/api/backlog/${backlogId}`, projectTask);
-    history.push(`/projectBoard/${backlogId}`);
+    try {
+      await axios.post(`/api/backlog/${backlogId}`, projectTask);
+      history.push(`/projectBoard/${backlogId}`);
+      dispatch({
+        type: GET_ERRORS,
+        payload: {}
+      });
+    } catch (ex) {
+      dispatch({
+        type: GET_ERRORS,
+        payload: ex.response.data
+      });
+    }
   };
