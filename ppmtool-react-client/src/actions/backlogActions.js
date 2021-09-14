1d98f3f68eb6a0e08e2a51a4afa340faab61c792
ahamilto99 <ajhamilton15@hotmail.com>
2021-09-14 13:14:30 -0400

    Complete Delete Project Task (front-end) feature

--- ppmtool-react-client/src/actions/backlogActions.js
+++ ppmtool-react-client/src/actions/backlogActions.js
@@ -1,28 +1,30 @@
-import axios from "axios";
-import { GET_ERRORS, GET_BACKLOG, GET_PROJECT_TASK } from "./types";
+import axios from 'axios';
+import {
+  GET_ERRORS,
+  GET_BACKLOG,
+  GET_PROJECT_TASK,
+  DELETE_PROJECT_TASK
+} from './types';
 
 //Fix bug with priority in Spring Boot Server, needs to check null first
-export const addProjectTask = (
-  backlog_id,
-  project_task,
-  history
-) => async dispatch => {
-  try {
-    await axios.post(`/api/backlog/${backlog_id}`, project_task);
-    history.push(`/projectBoard/${backlog_id}`);
-    dispatch({
-      type: GET_ERRORS,
-      payload: {}
-    });
-  } catch (err) {
-    dispatch({
-      type: GET_ERRORS,
-      payload: err.response.data
-    });
-  }
-};
+export const addProjectTask =
+  (backlog_id, project_task, history) => async (dispatch) => {
+    try {
+      await axios.post(`/api/backlog/${backlog_id}`, project_task);
+      history.push(`/projectBoard/${backlog_id}`);
+      dispatch({
+        type: GET_ERRORS,
+        payload: {}
+      });
+    } catch (err) {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+    }
+  };
 
-export const getBacklog = backlog_id => async dispatch => {
+export const getBacklog = (backlog_id) => async (dispatch) => {
   try {
     const res = await axios.get(`/api/backlog/${backlog_id}`);
     dispatch({
@@ -37,39 +39,46 @@ export const getBacklog = backlog_id => async dispatch => {
   }
 };
 
-export const getProjectTask = (
-  backlog_id,
-  pt_id,
-  history
-) => async dispatch => {
-  try {
-    const res = await axios.get(`/api/backlog/${backlog_id}/${pt_id}`);
-    dispatch({
-      type: GET_PROJECT_TASK,
-      payload: res.data
-    });
-  } catch (err) {
-    history.push("/dashboard");
-  }
-};
+export const getProjectTask =
+  (backlog_id, pt_id, history) => async (dispatch) => {
+    try {
+      const res = await axios.get(`/api/backlog/${backlog_id}/${pt_id}`);
+      dispatch({
+        type: GET_PROJECT_TASK,
+        payload: res.data
+      });
+    } catch (err) {
+      history.push('/dashboard');
+    }
+  };
 
-export const updateProjectTask = (
-  backlog_id,
-  pt_id,
-  project_task,
-  history
-) => async dispatch => {
-  try {
-    await axios.patch(`/api/backlog/${backlog_id}/${pt_id}`, project_task);
-    history.push(`/projectBoard/${backlog_id}`);
+export const updateProjectTask =
+  (backlog_id, pt_id, project_task, history) => async (dispatch) => {
+    try {
+      await axios.patch(`/api/backlog/${backlog_id}/${pt_id}`, project_task);
+      history.push(`/projectBoard/${backlog_id}`);
+      dispatch({
+        type: GET_ERRORS,
+        payload: {}
+      });
+    } catch (err) {
+      dispatch({
+        type: GET_ERRORS,
+        payload: err.response.data
+      });
+    }
+  };
+
+export const deleteProjectTask = (backlog_id, pt_id) => async (dispatch) => {
+  if (
+    window.confirm(
+      `You are deleting Project Task ${pt_id}. This action cannot be undone.`
+    )
+  ) {
+    await axios.delete(`/api/backlog/${backlog_id}/${pt_id}`);
     dispatch({
-      type: GET_ERRORS,
-      payload: {}
-    });
-  } catch (err) {
-    dispatch({
-      type: GET_ERRORS,
-      payload: err.response.data
+      type: DELETE_PROJECT_TASK,
+      payload: pt_id
     });
   }
 };
