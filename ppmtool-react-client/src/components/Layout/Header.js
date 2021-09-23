8f1103551984f886686739ba73b09f48609dabd9
ahamilto99 <ajhamilton15@hotmail.com>
2021-09-23 17:52:23 -0400

    Adding Landing, Login and Register components

--- ppmtool-react-client/src/components/Layout/Header.js
+++ ppmtool-react-client/src/components/Layout/Header.js
@@ -1,41 +1,42 @@
-import React, { Component } from "react";
+import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
 
 class Header extends Component {
   render() {
     return (
-      <nav className="navbar navbar-expand-sm navbar-dark bg-primary mb-4">
-        <div className="container">
-          <a className="navbar-brand" href="Dashboard.html">
+      <nav className='navbar navbar-expand-sm navbar-dark bg-primary mb-4'>
+        <div className='container'>
+          <Link className='navbar-brand' to='/'>
             Personal Project Management Tool
-          </a>
+          </Link>
           <button
-            className="navbar-toggler"
-            type="button"
-            data-toggle="collapse"
-            data-target="#mobile-nav"
+            className='navbar-toggler'
+            type='button'
+            data-toggle='collapse'
+            data-target='#mobile-nav'
           >
-            <span className="navbar-toggler-icon" />
+            <span className='navbar-toggler-icon' />
           </button>
 
-          <div className="collapse navbar-collapse" id="mobile-nav">
-            <ul className="navbar-nav mr-auto">
-              <li className="nav-item">
-                <a className="nav-link" href="/dashboard">
+          <div className='collapse navbar-collapse' id='mobile-nav'>
+            <ul className='navbar-nav mr-auto'>
+              <li className='nav-item'>
+                <Link className='nav-link' to='/dashboard'>
                   Dashboard
-                </a>
+                </Link>
               </li>
             </ul>
 
-            <ul className="navbar-nav ml-auto">
-              <li className="nav-item">
-                <a className="nav-link " href="register.html">
+            <ul className='navbar-nav ml-auto'>
+              <li className='nav-item'>
+                <Link className='nav-link' to='/register'>
                   Sign Up
-                </a>
+                </Link>
               </li>
-              <li className="nav-item">
-                <a className="nav-link" href="login.html">
+              <li className='nav-item'>
+                <Link className='nav-link' to='/login'>
                   Login
-                </a>
+                </Link>
               </li>
             </ul>
           </div>
